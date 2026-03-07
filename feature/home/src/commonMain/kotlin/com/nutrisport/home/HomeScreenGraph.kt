9f0e4dc716aef5bc83ee6526065da1e4db8c5f33
Deepak <deepak91313@gmail.com>
2026-03-08 05:21:35 +0530

    Bottom bar design created

--- feature/home/src/commonMain/kotlin/com/nutrisport/home/HomeScreenGraph.kt
+++ feature/home/src/commonMain/kotlin/com/nutrisport/home/HomeScreenGraph.kt
@@ -1,8 +1,42 @@
 package com.nutrisport.home
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.nutrisport.home.component.BottomBar
+import com.nutrisport.shared.Surface
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
+@Preview
 @Composable
 fun HomeGraphScreen() {
+    Scaffold(
+        containerColor = Surface
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier.fillMaxSize()
+                .padding(
+                    top = paddingValues.calculateTopPadding(),
+                    bottom = paddingValues.calculateBottomPadding()
+                )
+        ) {
+            Spacer(modifier = Modifier.weight(1f))
+            Box(
+                modifier = Modifier.padding(all = 12.dp)
+            ) {
+                BottomBar(
+                    selected = false,
+                    onSelect = { destination ->
 
+                    }
+                )
+            }
+        }
+    }
 }
