3818fc0beb0e2736a02dc4b1b7f4699c087daaed
Deepak <deepak91313@gmail.com>
2026-03-08 06:13:15 +0530

    Changed bg of status bar acc. to App theme

--- composeApp/src/androidMain/kotlin/org/deepak/nutrisport/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/deepak/nutrisport/MainActivity.kt
@@ -1,7 +1,9 @@
 package org.deepak.nutrisport
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
@@ -12,7 +14,16 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                Color.TRANSPARENT,
+                Color.TRANSPARENT
+            ),
+            navigationBarStyle = SystemBarStyle.light(
+                Color.TRANSPARENT,
+                Color.TRANSPARENT
+            )
+        )
         setContent {
             App()
         }
