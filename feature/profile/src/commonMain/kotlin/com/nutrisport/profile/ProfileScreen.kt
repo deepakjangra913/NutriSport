4e8ad10189e7f5dd194cbbd89a8f789c16da6b7e
Deepak <deepakjangra91313@gmail.com>
2026-03-12 05:44:05 +0530

    Commit Includes - Custom Component created for Profile Form - Added some properties in CustomTextField.kt

--- feature/profile/src/commonMain/kotlin/com/nutrisport/profile/ProfileScreen.kt
+++ feature/profile/src/commonMain/kotlin/com/nutrisport/profile/ProfileScreen.kt
@@ -1,8 +1,34 @@
 package com.nutrisport.profile
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.nutrisport.shared.Surface
+import com.nutrisport.shared.component.ProfileForm
 
 @Composable
 fun ProfileScreen() {
-
+    Column(
+        modifier = Modifier
+            .background(Surface)
+            .systemBarsPadding()
+    ) {
+        ProfileForm(
+            firstName = "Deepak",
+            onFirstNameChange = {},
+            lastName = "",
+            onLastNameChange = {},
+            email = "",
+            city = "",
+            onCityChange = {},
+            postalCode = null,
+            onPostalCodeChange = {},
+            address = "",
+            onAddressChange = {},
+            phoneNumber = "",
+            onPhoneNumberChange = {}
+        )
+    }
 }
