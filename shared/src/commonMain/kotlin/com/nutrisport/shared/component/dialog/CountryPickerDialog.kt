7feb4d7a1cb92e5e8e1681fc5f24cd067bcd0b54
Deepak <deepakjangra91313@gmail.com>
2026-03-13 22:28:24 +0530

    Commit Includes - WIP country code picker dialog

--- shared/src/commonMain/kotlin/com/nutrisport/shared/component/dialog/CountryPickerDialog.kt
+++ shared/src/commonMain/kotlin/com/nutrisport/shared/component/dialog/CountryPickerDialog.kt
@@ -2,37 +2,166 @@ package com.nutrisport.shared.component.dialog
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.ColorMatrix
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import com.nutrisport.shared.Alpha
+import com.nutrisport.shared.FontSize
 import com.nutrisport.shared.IconWhite
 import com.nutrisport.shared.Resources
+import com.nutrisport.shared.Surface
 import com.nutrisport.shared.SurfaceLighter
 import com.nutrisport.shared.SurfaceSecondary
+import com.nutrisport.shared.TextPrimary
+import com.nutrisport.shared.TextSecondary
+import com.nutrisport.shared.component.CustomTextField
 import com.nutrisport.shared.domain.Country
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
-fun CountryPickerDialog() {
+fun CountryPickerDialog(
+    modifier: Modifier = Modifier,
+) {
+    AlertDialog(
+        containerColor = Surface,
+        onDismissRequest = {
 
+        },
+
+        confirmButton = {
+            TextButton(
+                onClick = {},
+                colors = ButtonDefaults.textButtonColors(
+                    contentColor = TextSecondary,
+                    containerColor = Color.Transparent
+                )
+            ) {
+                Text(
+                    text = "Confirm",
+                    fontSize = FontSize.REGULAR,
+                    fontWeight = FontWeight.Medium
+                )
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = {},
+                colors = ButtonColors(
+                    containerColor = Surface,
+                    contentColor = TextPrimary.copy(alpha = Alpha.HALF),
+                    disabledContainerColor = Surface,
+                    disabledContentColor = Surface,
+                )
+            ) {
+                Text(
+                    text = "Cancel",
+                    fontSize = FontSize.REGULAR
+                )
+            }
+        },
+        modifier = modifier,
+        title = {
+            Text(
+                text = "Pick a Country",
+                fontSize = FontSize.EXTRA_MEDIUM,
+                color = TextPrimary
+            )
+        },
+        text = {
+            Column {
+                CustomTextField(
+                    value = "",
+                    onValueChange = {},
+                    placeholder = "Dial Code"
+                )
+
+            }
+        },
+        shape = RoundedCornerShape(size = 16.dp),
+        titleContentColor = Surface,
+        textContentColor = Surface
+    )
 }
 
 @Composable
-fun CountryCode(
+fun CountryPicker(
+    modifier: Modifier = Modifier,
     country: Country,
     isSelected: Boolean,
     onSelect: () -> Unit
-){
+) {
 
+    val saturation by animateFloatAsState(
+        targetValue = if (isSelected) 1f else 0f,
+        label = "SaturationAnimation"
+    )
+
+    val colorMatrix = remember(saturation) {
+        ColorMatrix().apply {
+            setToSaturation(saturation)
+        }
+    }
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .clickable {
+                onSelect()
+            }
+            .padding(vertical = 8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            modifier = Modifier.size(14.dp),
+            painter = painterResource(country.flag),
+            contentDescription = "Country Flag",
+            colorFilter = ColorFilter.colorMatrix(colorMatrix)
+        )
+
+        Spacer(modifier = Modifier.width(12.dp))
+
+        Text(
+            modifier = Modifier.weight(1f),
+            text = "+${country.dialCode} (${country.name})",
+            fontSize = FontSize.REGULAR,
+            color = TextPrimary,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+
+        Selector(
+            isSelected = isSelected
+        )
+    }
 }
 
 @Composable
@@ -66,10 +195,8 @@ private fun Selector(
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
-fun SelectorPreview() {
-    Selector(
-        isSelected = true
-    )
+fun CountryPickerDialogPreview() {
+    CountryPickerDialog()
 }
