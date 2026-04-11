6328e6112c08a956ae4e93f0aaf64836e6f4cf5a
Deepak <deepakjangra91313@gmail.com>
2026-04-11 22:34:54 +0530

    Commit Includes - WIP Checkout screen

--- feature/home/cart/checkout/src/commonMain/kotlin/com/nutrisport/checkout/CheckoutScreen.kt
+++ feature/home/cart/checkout/src/commonMain/kotlin/com/nutrisport/checkout/CheckoutScreen.kt
@@ -1,9 +1,95 @@
 package com.nutrisport.checkout
 
+import ContentWithMessageBar
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.nutrisport.shared.BebasNeueFont
+import com.nutrisport.shared.FontSize
+import com.nutrisport.shared.IconPrimary
+import com.nutrisport.shared.Resources
+import com.nutrisport.shared.Surface
+import com.nutrisport.shared.TextPrimary
+import org.jetbrains.compose.resources.painterResource
+import rememberMessageBarState
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CheckoutScreen(
+    totalAmount: Double,
+    navigateBack: () -> Unit
 ) {
+    val messageBarState = rememberMessageBarState()
 
+    Scaffold(
+        containerColor = Surface,
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "Checkout",
+                        fontSize = FontSize.LARGE,
+                        fontFamily = BebasNeueFont(),
+                        color = TextPrimary
+                    )
+                },
+                actions = {
+                    Text(
+                        text = "$${totalAmount}",
+                        fontSize = FontSize.EXTRA_MEDIUM,
+                        fontWeight = FontWeight.Medium,
+                        color = TextPrimary
+                    )
+                    Spacer(modifier = Modifier.width(16.dp))
+                },
+                navigationIcon = {
+
+                    IconButton(
+                        onClick = {
+                            navigateBack()
+                        }
+                    ) {
+                        Icon(
+                            painter = painterResource(Resources.Icon.BackArrow),
+                            contentDescription = "Back Arrow Icon",
+                            tint = IconPrimary
+                        )
+                    }
+
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Surface,
+                    scrolledContainerColor = Surface,
+                    navigationIconContentColor = IconPrimary,
+                    titleContentColor = TextPrimary,
+                    actionIconContentColor = IconPrimary
+                )
+            )
+        }
+    ) { paddingValues ->
+        ContentWithMessageBar(
+            modifier = Modifier.fillMaxSize()
+                .padding(
+                    top = paddingValues.calculateTopPadding(),
+                    bottom = paddingValues.calculateBottomPadding()
+                ),
+            messageBarState = messageBarState,
+            errorMaxLines = 2,
+            contentBackgroundColor = Surface
+        ) {
+
+        }
+    }
 }
