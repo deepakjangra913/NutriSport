c73d85055bed9ff9d3f9394153e58f8dbeede86e
Deepak <deepakjangra91313@gmail.com>
2026-04-11 21:28:30 +0530

    Commit Includes - Product search implementation completed for Category search screen

--- feature/home/categories/category_search/src/commonMain/kotlin/com/nutrisport/category_search/CategorySearchScreen.kt
+++ feature/home/categories/category_search/src/commonMain/kotlin/com/nutrisport/category_search/CategorySearchScreen.kt
@@ -1,28 +1,40 @@
 package com.nutrisport.category_search
 
 import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.nutrisport.shared.BebasNeueFont
+import com.nutrisport.shared.BorderIdle
 import com.nutrisport.shared.FontSize
 import com.nutrisport.shared.IconPrimary
 import com.nutrisport.shared.Resources
 import com.nutrisport.shared.Surface
+import com.nutrisport.shared.SurfaceLighter
 import com.nutrisport.shared.TextPrimary
 import com.nutrisport.shared.component.InfoCard
 import com.nutrisport.shared.component.LoadingCard
@@ -40,65 +52,67 @@ fun CategorySearchScreen(
     category: ProductCategory
 ) {
     val viewModel = koinViewModel<CategorySearchViewModel>()
-    val products by viewModel.products.collectAsStateWithLifecycle()
+    val filterProducts by viewModel.filterProducts.collectAsStateWithLifecycle()
+    val searchQuery by viewModel.searchQuery.collectAsStateWithLifecycle()
+    var searchBarVisible by mutableStateOf(false)
 
     Scaffold(
         containerColor = Surface,
         topBar = {
-            AnimatedContent(false) { visible ->
+            AnimatedContent(searchBarVisible) { visible ->
                 if (visible) {
-                    /* SearchBar(
-                         modifier = Modifier
-                             .padding(
-                                 horizontal = 12.dp
-                             ).fillMaxWidth(),
-                         colors = SearchBarDefaults.colors(
-                             containerColor = SurfaceLighter,
-                             dividerColor = BorderIdle
-                         ),
-                         expanded = false,
-                         onExpandedChange = {},
-                         inputField = {
-                             SearchBarDefaults.InputField(
-                                 modifier = Modifier
-                                     .fillMaxWidth(),
-                                 colors = TextFieldDefaults.colors(
-                                     unfocusedContainerColor = SurfaceLighter,
-                                     focusedContainerColor = SurfaceLighter
-                                 ),
-                                 query = searchQuery,
-                                 onQueryChange = viewModel::updatedSearchQuery,
-                                 expanded = false,
-                                 onExpandedChange = {},
-                                 onSearch = {},
-                                 placeholder = {
-                                     Text(
-                                         text = "Search here...",
-                                         color = TextPrimary,
-                                         fontSize = FontSize.REGULAR
-                                     )
-                                 },
-                                 trailingIcon = {
-                                     IconButton(
-                                         modifier = Modifier.size(14.dp),
-                                         onClick = {
-                                             if (searchQuery.isNotEmpty()){
-                                                 viewModel.updatedSearchQuery("")
-                                             }else{
-                                                 searchBarVisible = false
-                                             }
-                                         }) {
-                                         Icon(
-                                             painter = painterResource(Resources.Icon.Close),
-                                             contentDescription = "Close Icon",
-                                             tint = IconPrimary
-                                         )
-                                     }
-                                 }
-                             )
-                         },
-                         content = {}
-                     )*/
+                    SearchBar(
+                        modifier = Modifier
+                            .padding(
+                                horizontal = 12.dp
+                            ).fillMaxWidth(),
+                        colors = SearchBarDefaults.colors(
+                            containerColor = SurfaceLighter,
+                            dividerColor = BorderIdle
+                        ),
+                        expanded = false,
+                        onExpandedChange = {},
+                        inputField = {
+                            SearchBarDefaults.InputField(
+                                modifier = Modifier
+                                    .fillMaxWidth(),
+                                colors = TextFieldDefaults.colors(
+                                    unfocusedContainerColor = SurfaceLighter,
+                                    focusedContainerColor = SurfaceLighter
+                                ),
+                                query = searchQuery,
+                                onQueryChange = viewModel::updateSearchQuery,
+                                expanded = false,
+                                onExpandedChange = {},
+                                onSearch = {},
+                                placeholder = {
+                                    Text(
+                                        text = "Search here...",
+                                        color = TextPrimary,
+                                        fontSize = FontSize.REGULAR
+                                    )
+                                },
+                                trailingIcon = {
+                                    IconButton(
+                                        modifier = Modifier.size(14.dp),
+                                        onClick = {
+                                            if (searchQuery.isNotEmpty()) {
+                                                viewModel.updateSearchQuery("")
+                                            } else {
+                                                searchBarVisible = false
+                                            }
+                                        }) {
+                                        Icon(
+                                            painter = painterResource(Resources.Icon.Close),
+                                            contentDescription = "Close Icon",
+                                            tint = IconPrimary
+                                        )
+                                    }
+                                }
+                            )
+                        },
+                        content = {}
+                    )
                 } else {
                     TopAppBar(
                         title = {
@@ -122,7 +136,7 @@ fun CategorySearchScreen(
                         },
                         actions = {
                             IconButton(onClick = {
-//                                searchBarVisible = true
+                                searchBarVisible = true
                             }) {
                                 Icon(
                                     painter = painterResource(Resources.Icon.Search),
@@ -143,7 +157,7 @@ fun CategorySearchScreen(
             }
         },
         content = { paddingValues ->
-            products.DisplayResult(
+            filterProducts.DisplayResult(
                 modifier = Modifier.padding(
                     bottom = paddingValues.calculateBottomPadding(),
                     top = paddingValues.calculateTopPadding()
@@ -185,11 +199,13 @@ fun CategorySearchScreen(
                 },
                 onError = { message ->
                     InfoCard(
+                        modifier = Modifier.fillMaxSize(),
                         icon = Resources.Image.Cat,
                         title = "Oops!",
                         subTitle = message
                     )
-                }
+                },
+                transitionSpec = { fadeIn() togetherWith fadeOut() }
             )
         }
     )
--- feature/home/categories/category_search/src/commonMain/kotlin/com/nutrisport/category_search/CategorySearchViewModel.kt
+++ feature/home/categories/category_search/src/commonMain/kotlin/com/nutrisport/category_search/CategorySearchViewModel.kt
@@ -6,7 +6,14 @@ import androidx.lifecycle.viewModelScope
 import com.nutrisport.data.domain.ProductsRepository
 import com.nutrisport.shared.domain.ProductCategory
 import com.nutrisport.shared.util.RequestState
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.stateIn
 
 class CategorySearchViewModel(
@@ -14,11 +21,40 @@ class CategorySearchViewModel(
     private val savedStateHandle: SavedStateHandle
 ) : ViewModel() {
 
-    val products = productsRepository.readProductsByCategoryFlow(
+    private val products = productsRepository.readProductsByCategoryFlow(
         ProductCategory.valueOf(savedStateHandle.get<String>("category").orEmpty())
     ).stateIn(
         scope = viewModelScope,
         started = SharingStarted.WhileSubscribed(5000),
         initialValue = RequestState.Loading
     )
+
+    private var _searchQuery = MutableStateFlow("")
+    val searchQuery = _searchQuery.asStateFlow()
+
+    fun updateSearchQuery(value: String) {
+        _searchQuery.value = value
+    }
+
+    @OptIn(FlowPreview::class, ExperimentalCoroutinesApi::class)
+    val filterProducts = searchQuery
+        .debounce(500)
+        .flatMapLatest { query ->
+            if (query.isEmpty()) products
+            else {
+                if (products.value.isSuccess()) {
+                    flowOf(
+                        RequestState.Success(
+                            products.value.getSuccessData()
+                                .filter { it.title.lowercase().contains(query.lowercase()) })
+                    )
+                } else {
+                    products
+                }
+            }
+        }.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = RequestState.Loading
+        )
 }
