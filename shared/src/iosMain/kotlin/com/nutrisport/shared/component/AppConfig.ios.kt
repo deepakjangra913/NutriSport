02a08a98dd085772ba9c5f7a64c724ed4f146b63
Deepak <deepakjangra91313@gmail.com>
2026-04-15 04:54:59 +0530

    Add iOS PayPal config with secure secrets handling

--- shared/src/iosMain/kotlin/com/nutrisport/shared/component/AppConfig.ios.kt
+++ shared/src/iosMain/kotlin/com/nutrisport/shared/component/AppConfig.ios.kt
@@ -1,8 +1,12 @@
 package com.nutrisport.shared.component
 
+import platform.Foundation.NSBundle
+
 actual object AppConfig {
     actual val paypalClientId: String
-        get() = ""
+        get() = NSBundle.mainBundle.objectForInfoDictionaryKey("PaypalClientID") as? String
+            ?: error("PAYPAL_CLIENT_ID missing from Info.plist")
     actual val paypalSecret: String
-        get() = ""
+        get() = NSBundle.mainBundle.objectForInfoDictionaryKey("PaypalSecret") as? String
+            ?: error("PAYPAL_SECRET missing from Info.plist")
 }
